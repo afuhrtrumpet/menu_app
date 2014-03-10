9306e8d9fc2d8a09d0ae34c400618a36fce96e18
Alex Fuhr <afuhrtrumpet>
2014-03-09 20:41:40 -0400

    Finished beginning pages and layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,9 @@ describe "StaticPages" do
 
 	describe "About page" do
 
-		it "should have the content 'Help'" do
+		it "should have the content 'About'" do
 			visit '/about'
-			expect(page).to have_content('Help')
+			expect(page).to have_content('About')
 		end
 	end
 end
